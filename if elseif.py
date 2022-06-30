8a51dabb1cdd903c7b280cd682df16028f548f70
yih8 <80453336+yih8@users.noreply.github.com>
2022-06-30 02:00:27 +0200

    if elif2

--- if elseif.py
+++ if elseif.py
@@ -1,8 +1,8 @@
-a="Bachelor"
+a="Master"
 
 if a=="Bachelor":
     print("Coburg: Engineering Physics")
-elif a==Master:
+elif a=="Master":
     print("Deggendorf: Electrical Engineering and information technology")
 else:
     print("Nein")
